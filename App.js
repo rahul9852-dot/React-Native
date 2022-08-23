1acb3ae01f574238bc00d45ef152efc5450eb678
rahul9852-dot <srayesh5432@gmail.com>
2022-08-23 22:07:24 +0530

    fix for yt-video uploading

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,SafeAreaView,ScrollView, Button,Text, View,Image } from 'react-native';
 import { Video, AVPlaybackStatus } from 'expo-av';
 import { Card } from 'react-native-paper';
+import { WebView } from 'react-native-webview';
 import * as React from 'react';
 
 export default function App() {
@@ -18,17 +19,51 @@ export default function App() {
           source={require("./assets/Avatar.png")}
         />
       </View>
+       <Card style={styles.card1}>
+          <Video
+            ref={video}
+            style={styles.video}
+            source={require("./iphone13.mp4")}
+            useNativeControls
+            // resizeMode="contain"
+            isLooping
+            onPlaybackStatusUpdate={status => setStatus(() => status)}
+          />
+      <Text style={styles.noramlText}>IPHONE SAVING PLAN</Text>
+      <Text style={styles.boldText}>Save for the next iphone 
+        <Text style={{color:"green"}}> and get 10% cashback</Text>
+      </Text>
+           
+              <View>
+                <Image 
+                  style={styles.Image1}
+                  source={require("./assets/Truck.png")}
+                />
+                <Text style={styles.delivery}>Priority Delivery</Text>
+                </View>
+                <Image 
+                  style={styles.repeatedImage}
+                  source={require("./assets/Charger.png")}
+                />
+                <Image 
+                  style={styles.repeatedImage}
+                  source={require("./assets/gift.png")}
+                />
+      </Card>
       <Card style={styles.card}>
       <Video
         ref={video}
         style={styles.video}
         source={require("./demo.mp4")}
         useNativeControls
-        resizeMode="contain"
+        // resizeMode="contain"
         isLooping
         onPlaybackStatusUpdate={status => setStatus(() => status)}
       />
-      
+      <Image 
+          style={styles.logo}
+          source={require("./assets/makeMyTrip.jpg")}
+        />
       <Text style={styles.noramlText}>MAKE MY TRIP SAVING PLAN</Text>
       <Text style={styles.boldText}>Save for the next vacation 
         <Text style={{color:"green"}}> and get 10% cashback</Text>
@@ -52,9 +87,18 @@ const styles = StyleSheet.create({
       flex:1,
   },
   card:{
-    marginTop:10,
+    marginTop:20,
     width: 300,
-    alignSelf:"center"
+    alignSelf:"center",
+    borderRadius:10,
+    background: "#FFFFFF", 
+  },
+  card1:{
+    marginTop: 20,
+    width: 300,
+    alignSelf:"center",
+    borderRadius:10,
+    background: "#FFFFFF", 
   },
   welcomeMsg: {
     marginTop: 80,
@@ -68,21 +112,49 @@ const styles = StyleSheet.create({
     marginLeft: 50,
     fontSize: 30,
   },
-  imageStyles:{
-    padding:0,
-    marginLeft:250,
+  conatinerSubLogo:{
+    display:"flex",
+    flexDirection: "row",            /* default value; can be omitted */
+    flexWrap: "nowrap",         /* default value; can be omitted */
+    justifyContent: "space-between",  /* switched from default (flex-start, see below) */
+    // background-color: lightyellow;
   },
-  // videoContainer: {
-  //   alignSelf: 'center',
-  //   width: 300,
-  //   borderWidth: 5,
-  //   borderColor: 'grey',
-  //   borderRadius:10
-  // },
+  Image1:{
+    width: 100,
+    height: 65,
+    backgroundColor:"#EFEFEF",
+    margin: 20,
+    borderRadius: 9
+  },
+  imageStyles:{
+    marginLeft:280,
+    position:"absolute",
+    marginTop: 100
+  },
   video:{
     alignSelf:'center',
-    width:350,
-    height:532,
+    width:300,
+    height:400,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    position:"relative"
+    // borderWidth:5
+  },
+  logo:{
+     position: 'absolute',
+      left: 15,
+      bottom: 170,
+      borderRadius:10,
+      width: 50,
+      height: 50,
+  },
+  subLogo:{
+     display:"flex",
+    //  width: 100,
+    //  height: 100,
+     backgroundColor: "gray",
+
+
   },
   buttons: {
     flexDirection: 'row',
@@ -95,6 +167,12 @@ const styles = StyleSheet.create({
     fontSize: 18,
     lineHeight: 12,
     // fontWeight:400
+  },
+  delivery:{
+    color: "#6F767E",
+    fontSize: 15,
+    lineHeight: 2,
+    padding: 17
   },
   boldText:{
     display:'flex',
